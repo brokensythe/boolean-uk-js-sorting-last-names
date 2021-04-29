76312b5d36af777c136b7dfab65e6c8594f4a8fc
brokensythe <ade_1.0@live.co.uk>
2021-04-29 09:10:22 +0100

    some observations

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const sortedNames = names.sort()
 
 const nameIndexPosition = names.indexOf(yourName)
 
-const uppercaseNames = for (const name of names) {
-    console.log(names[names.indexOf(name)].toUpperCase())
-}
+// const uppercaseNames = for (const name of names) {
+//     console.log(names[names.indexOf(name)].toUpperCase())
+// } this does not work because this code would need to be a function 
 
 // indexing number === names.indexOf("Mason") or any other name
 
 // yourName is a hacky variable as it seems to change the list positioning, a better way would be:
 // yourName = prompt(`What is your name???`)
 // pushYourName = names.push(yourName)
+
+
 
